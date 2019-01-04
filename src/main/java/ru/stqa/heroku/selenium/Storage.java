e95551a3a0faa1e5ed9b4de480a0ead4f1e15cdd
Alexei Barantsev <barancev@gmail.com>
2019-01-04 15:02:57 +0300

    Implementing Storage factory

--- src/main/java/ru/stqa/heroku/selenium/Storage.java
+++ src/main/java/ru/stqa/heroku/selenium/Storage.java
@@ -1,11 +1,24 @@
 package ru.stqa.heroku.selenium;
 
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public interface Storage {
+  Map<String, Supplier<Storage>> storageSuppliers = ImmutableMap.of(
+    "hibernate", HibernateStorage::getInstance
+    //"fauna", () -> FaunaStorage.getInstance(),
+    //"inmemory", () -> InMemoryStorage.getInstance()
+  );
   static Storage getInstance() {
-    return HibernateStorage.getInstance();
+    return storageSuppliers.get("hibernate").get();
   }
 
-  public <R> R inSession(Function<StorageSession, R> run);
+  static Storage getInstance(String type) {
+    return storageSuppliers.get(type).get();
+  }
+
+  <R> R inSession(Function<StorageSession, R> run);
 }
